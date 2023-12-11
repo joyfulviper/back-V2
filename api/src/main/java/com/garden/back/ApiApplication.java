3670130360a39bcb5fb918e3de8d2a4af0537746
joyfulviper <kimsk3113@naver.com>
2023-12-12 04:11:29 +0900

    asdf

--- api/src/main/java/com/garden/back/ApiApplication.java
+++ api/src/main/java/com/garden/back/ApiApplication.java
@@ -2,6 +2,12 @@ package com.garden.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +16,25 @@ public class ApiApplication {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Configuration
+    static class Config {
+
+        @Bean
+        public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+            return new ShallowEtagHeaderFilter();
+        }
+    }
+
+    @RestController
+    static class Etag {
+
+        @GetMapping("/etag")
+        public ResponseEntity<String> etag() {
+//            String[] s = {"hello", "hello2"};
+//            Random random = new SecureRandom();
+//            int n = random.nextInt(2);
+
+            return ResponseEntity.ok("etag");
+        }
+    }
 }
